3be2c0e843ac282d489c11ad4d6edb32607840aa
fan <1206203790@qq.com>
2025-11-18 17:23:29 +0800

    feat: 新增商品接口

--- backend/web/controllers/product_controller.go
+++ backend/web/controllers/product_controller.go
@@ -38,3 +38,25 @@ func (p *ProductController) PostUpdate()  {
 	p.Ctx.Redirect("/product/all")
 }
 
+// 请求商品新增页面
+func (p *ProductController) GetAdd() mvc.View {
+	return mvc.View{
+		Name: "product/add.html",
+	}
+}
+
+// 新增商品逻辑
+func (p *ProductController) PostAdd()  {
+	product := &datamodels.Product{}
+	p.Ctx.Request().ParseForm()
+	dec := common.NewDecoder(&common.DecoderOptions{TagName: "gf_test"})
+	if err := dec.Decode(p.Ctx.Request().Form, product); err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	_, err := p.ProductService.InsertProduct(product)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	p.Ctx.Redirect("/product/all")
+}
