4a758d3ef948ff4639c4dd11ac3c65b9f0f123c4
fan <1206203790@qq.com>
2025-11-18 16:36:34 +0800

    feat: product_controller中新增修改商品方法

---
+++ backend/web/controllers/product_controller.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"github.com/scnlk12/high-concurrency-flash-sale-system/common"
+	"github.com/scnlk12/high-concurrency-flash-sale-system/datamodels"
+	"github.com/scnlk12/high-concurrency-flash-sale-system/services"
+)
+
+type ProductController struct {
+	Ctx iris.Context
+	ProductService services.IProductService
+}
+
+func (p *ProductController) GetAll() mvc.View {
+	productArr, _ := p.ProductService.GetAllProduct()
+	return mvc.View{
+		Name: "product/view.html",
+		Data: iris.Map{
+			"productArray": productArr,
+		},
+	}
+}
+
+// 修改商品
+func (p *ProductController) PostUpdate()  {
+	product := &datamodels.Product{}
+	p.Ctx.Request().ParseForm()
+	dec := common.NewDecoder(&common.DecoderOptions{TagName: "gf_test"})
+	if err := dec.Decode(p.Ctx.Request().Form, product); err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	err := p.ProductService.UpdateProduct(product)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	p.Ctx.Redirect("/product/all")
+}
+
